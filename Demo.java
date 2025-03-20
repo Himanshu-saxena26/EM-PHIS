4825a5934a7f9a3a1f25aaf4895f0d91267df91a
Himanshu-saxena26 <himanshu.saxena@dedalus.com>
2025-03-20 19:14:02 +0530

    Modified file

---
+++ Demo.java
@@ -0,0 +1,6 @@
+Demo
+The error you're seeing occurs
+ because in PowerShell, the ls command is an
+  alias for the Get-ChildItem cmdlet, but the -a
+   option is not valid in PowerShell. 
+In PowerShell, the correct option to show hidden files is -Force.
